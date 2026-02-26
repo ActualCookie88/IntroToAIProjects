1b1951ef1c767648d2ee732145df2ec9c9d07a59
Bongo <kekoa5254@gmail.com>
2026-02-25 18:49:39 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+/* ////////////////////////////////////////////////////////////////////
+
+MAIN
+
+//////////////////////////////////////////////////////////////////// */
+
+int main() {
+    string fileName;
+    int choice;
+    
+    cout << "Enter dataset filename: ";
+    cin >> fileName;
+
+
+
+    return 0;
+}
